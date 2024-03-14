a724e3a4ba251be4b43133590649ac1ffb5a248c
mayrang <pkss0626@naver.com>
2024-03-14 10:42:27 +0900

    before login home page 디자인

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
-
+import styles from "./Home.module.css"
 export default function BeforeLoginHomePage(){
     return (
-        <div className="flex items-center justify-center w-screen h-screen gap-4">
-            <h1 className=" font-extrabold text-4xl ">Travel Map</h1>
-            <Link href={"/signin"} className="text-xl">로그인</Link>
+        <div className={styles.main}>
+            <div className={styles.titleSection}>
+                <h1 className={styles.mainTitle}>Travel Map</h1>
+                <span className={styles.subTitle}>AI를 통해 쉽게 여행 계획을 짜보세요!</span>
+            </div>
+
+            <Link href={"/signin"} className={styles.startButton}>로그인</Link>
         </div>
     )
 }
