9e89c4791a1994604a4504772ce5cce4ef3d46da
mayrang <pkss0626@naver.com>
2024-03-17 23:35:29 +0900

    여행추가 버튼

--- src/app/(afterLogin)/_component/MobileHeader.tsx
+++ src/app/(afterLogin)/_component/MobileHeader.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import styles from "./MobileHeader.module.css";
 import BackButton from "@/app/_component/BackButton";
 import MenuButton from "@/app/_component/MenuButton";
+import { usePathname } from "next/navigation";
 
 export default function MobileHeader() {
+  const pathname = usePathname();
+  console.log("header", pathname);
   return (
     <header className={styles.header}>
       <BackButton />
-      <div>타이틀</div>
+      {pathname === "/home" && <div>여행 목록</div>}
       <MenuButton />
     </header>
   );
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,6 +1,17 @@
+import Link from "next/link";
 import React from "react";
-
+import styles from "./home.module.css";
 export default function page() {
   console.log("check home");
-  return <div>home</div>;
+  return (
+    <div>
+      home
+      <Link href={"/add/travel"} className={styles.addButton}>
+        <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 4L12 20M20 12L4 12" stroke="#fff" strokeWidth="1.5" strokeLinecap="round" />
+        </svg>
+        여행 추가
+      </Link>
+    </div>
+  );
 }
