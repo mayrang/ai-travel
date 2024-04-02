034d193d8ded9dbbd7c2f134056b3affde7cdf54
mayrang <pkss0626@naver.com>
2024-04-02 15:50:35 +0900

    mapcontainer zoom center 변경"

--- src/app/(afterLogin)/new/travel/_component/MapContainer.tsx
+++ src/app/(afterLogin)/new/travel/_component/MapContainer.tsx
@@ -1,17 +1,31 @@
 "use client";
-import React, { useCallback, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import styles from "./MapContainer.module.css";
+import { useNewTravelStore } from "@/store/newTravel";
+// const markerOptions = {
+//   // 사용자 정의 마커 이미지를 지정합니다.
+//   icon: {
+//     url: "https://maps.google.com/mapfiles/ms/micons/blue.png",
+//     scaledSize: new window.google.maps.Size(30, 30), // 마커 크기 설정
+//   },
+// };
 
-export default function MapContainer() {
+const MapContainer: React.FC = () => {
   const { isLoaded } = useJsApiLoader({
     id: "travel_map",
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAP_API ?? "",
   });
-  const [map, setMap] = useState<google.maps.Map | null>(null);
+  const [lineCoordinates, setLineCoordinates] = useState<
+    google.maps.LatLng[] | null
+  >(null);
+  const [map, setMap] = React.useState<google.maps.Map | null>(null);
+  const [directions, setDirections] =
+    useState<google.maps.DirectionsResult | null>(null);
+  const { cities } = useNewTravelStore();
+  const center = { lat: cities[0]?.lat || 0, lng: cities[0]?.lng };
 
-  const center = { lat: 37.7749, lng: -122.4194 };
-  const onLoad = useCallback(function callback(map: google.maps.Map) {
+  const onLoad = React.useCallback(function callback(map: google.maps.Map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
@@ -19,9 +33,11 @@ export default function MapContainer() {
     setMap(map);
   }, []);
 
-  const onUnmount = useCallback(function callback(map: google.maps.Map) {
+  const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
     setMap(null);
   }, []);
+
+  console.log(directions);
 
   return isLoaded ? (
     <>
@@ -34,12 +50,26 @@ export default function MapContainer() {
           mapTypeControl: false,
           fullscreenControl: false,
         }}
-        zoom={10}
+        zoom={11}
         onLoad={onLoad}
         onUnmount={onUnmount}
-      ></GoogleMap>
+      >
+        <div
+          id="panel"
+          style={{
+            flex: "1",
+            padding: "20px",
+            height: "400px",
+            overflowY: "auto",
+            zIndex: 1100,
+            position: "fixed",
+          }}
+        ></div>
+      </GoogleMap>
     </>
   ) : (
     <div>로드 안됨</div>
   );
-}
+};
+
+export default MapContainer;
